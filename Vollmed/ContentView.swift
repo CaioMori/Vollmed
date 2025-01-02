04193ff130b4873ea2479aa9933a8d0d274de1ae
Caio Mori <caiomori22@gmail.com>
2025-01-02 17:45:20 -0300

    Initial Commit

---
+++ Vollmed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 02/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vollmed/VollmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VollmedApp.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct VollmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
