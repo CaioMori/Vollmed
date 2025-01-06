9da49d0cc717d4d811a8e84bda9c458c9ed2da81
Caio Mori <caiomori22@gmail.com>
2025-01-06 13:41:07 -0300

    Adiciona as views de SignIn e SignUp com layout e campos de entrada

--- Vollmed/Views/SignInView.swift
+++ Vollmed/Views/SignInView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 16.0) {
+            Image(.logo)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: 36.0, alignment: .center)
+            
+            Text("Olá")
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            Text("Preencha para acessar sua conta.")
+                .font(.title3)
+                .foregroundStyle(.gray)
+                .padding(.bottom)
+            
+            Text("Email")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            TextField("Insira seu email", text: $email)
+                .padding(14)
+                .background(Color.gray.opacity(0.25))
+                .cornerRadius(14.0)
+                .autocorrectionDisabled()
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+            
+            Text("Senha")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+            
+            SecureField("Digite sua senha", text: $password)
+                .padding(14)
+                .background(Color.gray.opacity(0.25))
+                .cornerRadius(14.0)
+                .keyboardType(.default)
+                .textInputAutocapitalization(.never)
+                .padding(.bottom)
+            
+            Button {
+                //
+            } label: {
+                ButtonView(text: "Entrar")
+            }
+            NavigationLink {
+                SignUpView()
+            } label: {
+                Text("Ainda não possui uma conta? Cadastre-se")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+
+        }
     }
 }
 
--- Vollmed/Views/SignUpView.swift
+++ Vollmed/Views/SignUpView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct SignUpView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading, spacing: 16.0) {
+                Image(.logo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: 36.0, alignment: .center)
+                    .padding(.vertical)
+                
+                Text("Olá, boas-vindas!")
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                Text("Insira seus dados para criar uma conta.")
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.bottom)
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .padding()
     }
 }
 
