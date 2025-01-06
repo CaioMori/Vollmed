b9409fc48da46ca8f61f6434734b2099087f7082
Caio Mori <caiomori22@gmail.com>
2025-01-06 13:41:01 -0300

    Adiciona as views de SignIn e SignUp

---
+++ Vollmed/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 06/01/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ Vollmed/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 06/01/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
