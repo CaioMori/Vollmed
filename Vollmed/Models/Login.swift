8ac573dc54644b97813df889b2a7c3119ccaad89
Caio Mori <caiomori22@gmail.com>
2025-01-08 12:50:29 -0300

    Adiciona modelo de requisição e resposta para login

---
+++ Vollmed/Models/Login.swift
@@ -0,0 +1,24 @@
+//
+//  Login.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 08/01/25.
+//
+
+import Foundation
+
+struct LoginRequest: Codable {
+    let email: String
+    let password: String
+    
+    enum CodingKeys: String, CodingKey {
+        case email
+        case password = "senha"
+    }
+}
+
+struct loginResponse: Identifiable, Codable {
+    let auth: Bool
+    let id: String
+    let token: String
+}
