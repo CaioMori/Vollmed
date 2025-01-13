573e86554c1a551f065d2d404a0f8dae50b6a986
Caio Mori <caiomori22@gmail.com>
2025-01-13 13:46:39 -0300

    feat(networking): adiciona HTTPClient para gerenciamento de requisições e refatora Endpoint para corpo opcional

---
+++ Vollmed/Networking/Endpoints/HomeEndpoint.swift
@@ -0,0 +1,16 @@
+//
+//  HomeEndpoint.swift
+//  Vollmed
+//
+//  Created by Caio Mori on 13/01/25.
+//
+
+import Foundation
+
+enum HomeEndpoint {
+    case getAllSpecialists
+}
+
+extension HomeEndpoint: Endpoint {
+    
+}
